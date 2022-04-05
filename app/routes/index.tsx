67b15486b69d00fc8fe4f35b8b61c470cdf3a6b7
André Casaleiro <andrefmc@users.noreply.github.com>
2022-04-06 00:21:19 +0100

    chore: migrate data into db

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,14 @@
+import { PrismaClient } from '@prisma/client'
+
+export async function loader() {
+  const prisma = new PrismaClient();
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+  await prisma.$disconnect();
+  return allUsers;
+}
 export default function Index() {
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
